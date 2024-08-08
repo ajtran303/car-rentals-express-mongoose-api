e8e91d6ddd026659ffc1d5d6d4ec2c2a50a3b4e5
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2024-08-08 18:43:35 -0400

    Add express config

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,10 @@ import mongoose, { ConnectOptions } from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
 
+// CONFIG
+
+dotenv.config()
+const app = express()
+app.use(cors())
+app.use(json())
+
