8b225a2382e9e1725a3dd676eb8ffd404b05a5d6
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2024-08-08 18:48:16 -0400

    Set up server

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,18 @@ const app = express()
 app.use(cors())
 app.use(json())
 
+const PORT = process.env.PORT || 6001
+const MONGO_URL = process.env.MONGO_URL || ""
+
+mongoose.set("strictQuery", false)
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  } as ConnectOptions)
+  .then(() => {
+    app.listen(PORT, () =>
+    console.log(`Connected at port:${PORT}`)
+    )
+  })
+  .catch(err => console.log(`${err}, did not connect`))
